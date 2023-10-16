7e7b1642b48d3d4a0c379e2ac8eabc43b657e83d
rohitashsingh89 <rohitashmtr2002@gmail.com>
2023-10-17 00:22:20 +0530

    Initial commit

---
+++ rohitashsingh.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 08, 2022 at 01:51 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rohitashsingh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `sno` int(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `phone_num` varchar(12) NOT NULL,
+  `msg` text NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`sno`, `name`, `email`, `phone_num`, `msg`, `date`) VALUES
+(1, 'Ganesh Kumar', 'ganeshkumar350@gmail', '9045210272', 'This is amazing website', '2022-12-01 20:43:56'),
+(2, 'Rohitash Singh', 'rohitashmtr2002@gmai', '08923354700', 'This is a good website', '2022-12-02 10:54:40'),
+(3, 'Pawan', 'pawan.kumar2022@glba', '8192013236', 'my experience is good on this site.', '2022-12-06 20:10:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `sno` int(11) NOT NULL,
+  `title` varchar(80) NOT NULL,
+  `slug` varchar(20) NOT NULL,
+  `content` varchar(1000) NOT NULL,
+  `tagline` varchar(50) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`sno`, `title`, `slug`, `content`, `tagline`, `date`) VALUES
+(1, 'flask tutorial edited', 'first-post', 'This is my first flask python blog. mm,,   ', 'This is  amazing python blog.', '2022-12-02 11:06:47'),
+(2, 'pandas blog', 'pandas-blog', 'This is a pandas tutorial in python.', 'This tutorial is going to be c', '2022-12-02 11:09:06'),
+(3, 'Let\'s learn Java Script', 'js-totorial', 'JavaScript is the world most popular lightweight, interpreted compiled programming language. It is also known as scripting language for web pages. It is well-known for the development of web pages; many non-browser environments also use it. JavaScript can be used for client-side developments as well as server-side developments.\r\n\r\nFeatures of JavaScript:  \r\n	JavaScript was created in the first place for DOM manipulation. Earlier websites were mostly static, after JS was created dynamic Web sites were made.\r\n	Functions in JS are objects. They may have properties and methods just like another object. They can be passed as arguments in other functions.\r\n	Can handle date and time.\r\n	Performs Form Validation although the forms are created using HTML.\r\n	No compiler is needed.\r\n   ', 'This is  amazing python blog.', '2022-12-02 11:15:30'),
+(5, 'Hello', 'first', ' This is for checking', 'world', '2022-12-07 03:32:58');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `sno` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
